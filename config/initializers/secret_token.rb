81ea51509294370ce3e766a0bb36c6fabdb0b88c
monimeli <mrizzuti@matrix.it>
2011-12-23 11:07:34 +0100

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Bastardometro::Application.config.secret_token = 'ce5dcb3c86d26ea1f659f7dd8c839bffcddc32a942ee5a6b2332f50947845c4ac3267c030e21cddb31a43d4c8ee4437c961ee6c92c42010ef03d50b4a422c9a5'
